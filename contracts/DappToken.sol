1de6a6c06405075c74db7fa192f060053acb73af
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-08-18 17:06:20 -0300

    added basic attributes

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,8 +1,10 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
+    string public name = 'Santi Token';
+    string public symbol = 'S22';
+    string public standard = 'Santi Token v1.0';
     uint256 public totalSupply;
-
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public{
--- test/DappToken.js
+++ test/DappToken.js
@@ -3,7 +3,22 @@ const DappToken = artifacts.require("DappToken");
 contract('DappToken' , (accounts) => {
     var tokenInstance;
 
-    it('sets the total supply amount upon deployment', () => {
+    it('initializes the contract with the correct vaulues', () => {
+        return DappToken.deployed().then( (instance) => {
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then( (name) => {
+            assert.equal(name, 'Santi Token', 'has the correct name');
+            return tokenInstance.symbol();
+        }).then( (symbol) => {
+            assert.equal(symbol, 'S22', 'has the correcct symbol');
+            return tokenInstance.standard();
+        }).then( (standard) => {
+            assert.equal(standard, 'Santi Token v1.0', 'has the correcct standard');
+        });
+    });
+
+    it('allocates the total supply upon deployment', () => {
         return DappToken.deployed().then( (instance) => {
             tokenInstance = instance;
             return tokenInstance.totalSupply();
