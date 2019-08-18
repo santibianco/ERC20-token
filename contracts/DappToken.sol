9a9d15aad3e20ad798c4502003362cc7d5059fc2
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-08-18 16:56:27 -0300

    added initial supply

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,14 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract DappToken {
-    // Set the total number of tokens
-    // Read the total number of tokens
     uint256 public totalSupply;
 
-    // Constructor
-    constructor() public{
-        totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public{
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
+
+
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,13 +1,17 @@
 const DappToken = artifacts.require("DappToken");
 
 contract('DappToken' , (accounts) => {
-    
+    var tokenInstance;
+
     it('sets the total supply amount upon deployment', () => {
         return DappToken.deployed().then( (instance) => {
-            return instance.totalSupply();
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
         }).then( (totalSupply) => {
             assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1.000.000');
-        })
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then((admingBalance) => {
+            assert.equal(admingBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account');
+        });
     })
-
 });
