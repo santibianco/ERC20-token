d4ce728bae5d3a799c07fd9bc98d0e93b1fcf129
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-08-19 23:34:02 -0300

    added transfer function and tests

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,10 +7,26 @@ contract DappToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     constructor(uint256 _initialSupply) public{
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[msg.sender] >= _value, 'Not enough tokens');
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 
 }
