e2c035959b5c803c66573413a8d9c72443a41b7d
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-09-02 23:59:33 -0300

    added function to buy tokens

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -1,16 +1,33 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 import "./MyToken.sol";
 
 contract TokenSale {
+
+  using SafeMath for uint256;
+
   address admin;
   MyToken public tokenContract;
   uint256 public tokenPrice;
+  uint256 public tokensSold;
+
+  event Sell(
+    address indexed _buyer,
+    uint256 _amount
+  );
 
   constructor(MyToken _tokenContract, uint256 _tokenPrice) public {
     admin = msg.sender;
     tokenContract = _tokenContract;
     tokenPrice = _tokenPrice;
+  }
+
+  function buyTokens(uint256 _numberOfTokens) public payable {
+    require(msg.value == _numberOfTokens.mul(tokenPrice), 'check value sent');
+    require(tokenContract.balanceOf(address(this)) >= _numberOfTokens, 'not enough tokens for that purchase');
+    require(tokenContract.transfer(msg.sender, _numberOfTokens), 'transfer failed');
+    tokensSold = tokensSold.add(_numberOfTokens);
+    emit Sell(msg.sender, _numberOfTokens);
   }
 
 }
--- test/token_sale.js
+++ test/token_sale.js
@@ -1,8 +1,15 @@
 const TokenSale = artifacts.require("TokenSale");
+const MyToken = artifacts.require("MyToken");
+
 
 contract('TokenSale', function(accounts) {
-  var tokenSaleInstance;
-  var tokenPrice = 1000000000000000; //wei
+  let tokenSaleInstance;
+  let tokenInstance;
+  const buyer = accounts[1];
+  const admin = accounts[0]
+  const tokensAvailable = 750000;
+  const tokenPrice = 1000000000000000; //wei
+  const numberOfTokens = 10;
 
   it("initializes the contract with the correct values", () => {
     return TokenSale.deployed().then( (instance) => {
@@ -18,4 +25,37 @@ contract('TokenSale', function(accounts) {
       assert.equal(price, tokenPrice, "sets the token price correctly");
     });
   });
+
+  it('facilitates token buying', () => {
+    return MyToken.deployed().then( (instance) => {
+      tokenInstance = instance;
+      return TokenSale.deployed();
+    }).then( (instance) => {
+      tokenSaleInstance = instance;
+      return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, {from: admin});
+    }).then( (recepit) => {  
+      return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: numberOfTokens * tokenPrice})  
+    }).then( (receipt) => {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Sell', 'the one triggered is the Transfer event');
+      assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the buyer account');
+      assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+      return tokenSaleInstance.tokensSold();
+    }).then( (amount) => {
+      assert.equal(amount.toNumber(), numberOfTokens, "increments the number of tokens sold");
+      return tokenInstance.balanceOf(buyer);
+    }).then((balance) => {
+      assert.equal(balance.toNumber(), numberOfTokens, "increases the buyer balance after the purchase");
+      return tokenInstance.balanceOf(tokenSaleInstance.address);
+    }).then((balance) => {
+      assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens, "decreases the token sale contract balance after the purchase");
+      return tokenSaleInstance.buyTokens.call(numberOfTokens, {from: buyer, value: 1});
+    }).then(assert.fail).catch( (error) => {
+      assert(error.message.indexOf('revert') >= 0, 'avoids sending incorrect value');
+      return tokenSaleInstance.buyTokens.call(tokensAvailable + 1, {from: buyer, value: numberOfTokens * tokenPrice});
+    }).then(assert.fail).catch( (error) => {
+      assert(error.message.indexOf('revert') >= 0, 'avoids buying more tokens than available');
+    });
+  });
+
 });
