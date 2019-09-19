42b310b072174ef8357a0b2dfac9286e55835c85
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-09-19 19:39:54 -0300

    ready to buy migration

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,10 +1,10 @@
 const MyToken = artifacts.require("MyToken");
 const TokenSale = artifacts.require("TokenSale");
 
-module.exports = function(deployer) {
-  deployer.deploy(MyToken, 1000000).then( () =>{
-    const tokenPrice = 1000000000000000; //wei
-    return deployer.deploy(TokenSale, MyToken.address, tokenPrice);
-  });
-  
+module.exports = async (deployer, network, accounts) => {
+  await deployer.deploy(MyToken, 1000000);
+  tokenInstance = await MyToken.deployed();
+  const tokenPrice = 1000000000000000; //wei
+  await deployer.deploy(TokenSale, MyToken.address, tokenPrice);
+  tokenInstance.transfer(TokenSale.address, 750000, {from: accounts[0]})
 };
